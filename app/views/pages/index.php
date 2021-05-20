8edc914f0d37031dc0f674645d0b71d9745a1609
arianrah <arianrahimi@icloud.com>
2021-05-20 12:54:43 -0700

    Cleanup and ready for packaging. -base completed.

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,8 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
-<ul>
-  <?php foreach ($data['posts'] as $post) : ?>
-    <li><?php echo $post->title; ?></li>
-  <?php endforeach; ?>
-</ul>
-<?php require APPROOT . '/views/inc/footer.php'; ?>
+<o>This is the EZPHP framework. Config and setup found on ~/README.md</p>
+  <?php require APPROOT . '/views/inc/footer.php'; ?>
